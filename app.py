0001e35159bd880d432ad26756f8ac15b2266f81
Fedor <fedor@agafonov.org>
2020-07-24 17:13:41 +0300

    init repo

--- app.py
+++ app.py
@@ -80,36 +80,36 @@ class Student(db.Model):
     book = db.relationship("Booking")
 
 
-with open('data/teachers.json', 'r') as teach:
-    get_data = json.load(teach)
-
-for item in get_data:
-    travel = 'travel' in get_data[item['id']]['goals']
-    relocate = 'relocate' in get_data[item['id']]['goals']
-    study = 'study' in get_data[item['id']]['goals']
-    program = 'program' in get_data[item['id']]['goals']
-    work = 'work' in get_data[item['id']]['goals']
-    gender = item['gender']
-    teacher = Teachers(name=item['name'], about=item['about'], rating=item['rating'], picture=item['picture'], price=item['price'], gender=gender, travel=travel, relocate=relocate, study=study, program=program, work=work)
-    db.session.add(teacher)
-
-for days in get_data[0]['free']:
-    print(days)
-    day = Day(week_day=days)
-    db.session.add(day)
-
-for time in get_data[0]['free']['mon']:
-    print(time)
-    times = Times(time=time)
-    db.session.add(times)
-
-db.session.commit()
-
-
-# db.session.query(Teachers).filter(Teachers.id > 12).delete()
-# db.session.query(Times).filter(Times.id > 8).delete()
-# db.session.query(Day).filter(Day.id > 7).delete()
+# with open('data/teachers.json', 'r') as teach:
+#     get_data = json.load(teach)
+#
+# for item in get_data:
+#     travel = 'travel' in get_data[item['id']]['goals']
+#     relocate = 'relocate' in get_data[item['id']]['goals']
+#     study = 'study' in get_data[item['id']]['goals']
+#     program = 'program' in get_data[item['id']]['goals']
+#     work = 'work' in get_data[item['id']]['goals']
+#     gender = item['gender']
+#     teacher = Teachers(name=item['name'], about=item['about'], rating=item['rating'], picture=item['picture'], price=item['price'], gender=gender, travel=travel, relocate=relocate, study=study, program=program, work=work)
+#     db.session.add(teacher)
+#
+# for days in get_data[0]['free']:
+#     print(days)
+#     day = Day(week_day=days)
+#     db.session.add(day)
+#
+# for time in get_data[0]['free']['mon']:
+#     print(time)
+#     times = Times(time=time)
+#     db.session.add(times)
+#
 # db.session.commit()
+
+
+db.session.query(Teachers).filter_by(Teachers.id > 12).delete()
+db.session.query(Times).filter_by(Times.id > 8).delete()
+db.session.query(Day).filter_by(Day.id > 7).delete()
+db.session.commit()
 
 
 # get end
