0b9e8c555827ad1ebe2bca25dff72456633bccc7
Fedor <fedor@agafonov.org>
2020-07-24 15:11:19 +0300

    init repo

--- app.py
+++ app.py
@@ -80,39 +80,40 @@ class Student(db.Model):
     book = db.relationship("Booking")
 
 
-teacher = db.session.query(Teachers).all()
-db.session.delete(teacher)
+# teacher = db.session.query(Teachers).all()
+# db.session.delete(teacher)
+#
+# time = db.session.query(Times).all()
+# db.session.delete(time)
+#
+# day = db.session.query(Day).all()
+# db.session.delete(day)
+# db.session.commit()
 
-time = db.session.query(Times).all()
-db.session.delete(time)
-
-day = db.session.query(Day).all()
-db.session.delete(day)
-db.session.commit()
 # with open('data/teachers.json', 'r') as teach:
 #     get_data = json.load(teach)
 #
-# for item in get_data:
-#     travel = 'travel' in get_data[item['id']]['goals']
-#     relocate = 'relocate' in get_data[item['id']]['goals']
-#     study = 'study' in get_data[item['id']]['goals']
-#     program = 'program' in get_data[item['id']]['goals']
-#     work = 'work' in get_data[item['id']]['goals']
-#     gender = item['gender']
-#     teacher = Teachers(name=item['name'], about=item['about'], rating=item['rating'], picture=item['picture'], price=item['price'], gender=gender, travel=travel, relocate=relocate, study=study, program=program, work=work)
-#     db.session.add(teacher)
-#
-# for days in get_data[0]['free']:
-#     print(days)
-#     day = Day(week_day=days)
-#     db.session.add(day)
-#
-# for time in get_data[0]['free']['mon']:
-#     print(time)
-#     times = Times(time=time)
-#     db.session.add(times)
-#
-# db.session.commit()
+for item in get_data:
+    travel = 'travel' in get_data[item['id']]['goals']
+    relocate = 'relocate' in get_data[item['id']]['goals']
+    study = 'study' in get_data[item['id']]['goals']
+    program = 'program' in get_data[item['id']]['goals']
+    work = 'work' in get_data[item['id']]['goals']
+    gender = item['gender']
+    teacher = Teachers(name=item['name'], about=item['about'], rating=item['rating'], picture=item['picture'], price=item['price'], gender=gender, travel=travel, relocate=relocate, study=study, program=program, work=work)
+    db.session.add(teacher)
+
+for days in get_data[0]['free']:
+    print(days)
+    day = Day(week_day=days)
+    db.session.add(day)
+
+for time in get_data[0]['free']['mon']:
+    print(time)
+    times = Times(time=time)
+    db.session.add(times)
+
+db.session.commit()
 
 # get end
 
